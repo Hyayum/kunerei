f7059435546bfdc63ce63848f70b45c50ea50a20
Timireno <hinomefi@gmail.com>
2024-11-04 04:34:25 +0900

    データ取得の構造を変えてみる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,18 @@ import MainPage from "./MainPage";
 import { API_URL } from "@/config";
 import { Suspense } from "react";
 
+const fetchData = async () => {
+  try {
+    const response = await fetch(API_URL, { cache: "no-cache" });
+    const data = await response.json();
+    return data;
+  } catch {
+    return [];
+  }
+};
+
 export default async function Home() {
-  const response = await fetch(API_URL, { cache: "no-cache" });
-  const data = await response.json();
+  const data = await fetchData();
   return (
     <ClientLayout>
       <Suspense>
